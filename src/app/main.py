644db9542e30bba35c2f8718c4d3cab05924476a
ravee <1819318+Raveendran@users.noreply.github.com>
2024-04-07 14:32:15 +1000

    Update main.py

--- src/app/main.py
+++ src/app/main.py
@@ -3,10 +3,6 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# ravee added
-from azure.identity import DefaultAzureCredential
-from azure.keyvault.secrets import SecretClient
-
 app = FastAPI()
 
 app.add_middleware(
